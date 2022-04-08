44468c263b413f31aaf5547434697b13f23d4c4d
loraigwe <lora.igwe@ucdconnect.ie>
2022-04-08 13:00:22 +0100

    new changes

--- main.c
+++ main.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
-<<<<<<< HEAD
-#include "twitter_create.h"
 #include "twitter_create.c"
-
-
-=======
-#include "twitter_create.c"
->>>>>>> 3e781a420e9ccd0d9eb08e4929daa8c9ea7be31e
 
 
 int main() {
@@ -16,14 +9,14 @@ int main() {
 
     //implement here the code to print the users
     // for each user you need to print the username, the number of followers and the number of users that the current user is following
-    user *previousPtr = NULL;
-    user *currentPtr = twitter_system.users;
-    user *nextPtr = currentPtr->next_user;
+    // user *previousPtr = NULL;
+    // user *currentPtr = twitter_system.users;
+    // user *nextPtr = currentPtr->next_user;
 
-    // while(nextPtr!=NULL){
-        printf("%s",currentPtr->username);
-    // }
-
+    while(twitter_system.users!=NULL){
+        printf("%s",twitter_system.users->username);
+        twitter_system.users = twitter_system.users->next_user;
+    }
 
 
 }
--- twitter_create.c
+++ twitter_create.c
@@ -5,13 +5,24 @@
 #include <stdio.h>
 #include <string.h>
 #include "twitter_create.h"
+#include <stdlib.h>
+
 
 void create_twitter_system(twitter * twitter_system){
-
+    int num;
     //to be completed
     twitter_system->users = NULL;
-    user *newuser = malloc(sizeof(user));
    
+    printf("How many users do you want to create: ");
+    scanf("%d", &num);
+
+    user *previousPtr = NULL;
+
+    // user *nextPtr = currentPtr->next_user;
+
+    for (int i =0; i < num; i++){
+    user *currentPtr = twitter_system->users;
+    user *newuser = malloc(sizeof(user));
     printf( "Enter your username: " );
     char name[USR_LENGHT];
     // gets(name);
@@ -19,9 +30,18 @@ void create_twitter_system(twitter * twitter_system){
     strcpy(newuser->username,name);
     newuser->num_followers=0;
     newuser->num_following=0;
-    newuser->next_user = twitter_system->users;
-    twitter_system->users = newuser;
+    if(previousPtr==NULL){
+        newuser->next_user = twitter_system->users;
+        twitter_system->users = newuser;
+    }
+    else {
+        previousPtr->next_user = newuser;
+        newuser->next_user = currentPtr;
+        
+    }
     // printf("%s",twitter_system->users->username);
-
+    twitter_system->users = previousPtr;
+    }
+    
 
 }
