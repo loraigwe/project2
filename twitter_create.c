2be3752053c050b9775f7e07100b6d57320db3c6
sujingdong <sujingdong07@gmail.com>
2022-04-26 17:17:24 +0100

    minor change

--- twitter_create.c
+++ twitter_create.c
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-// tweet *head = NULL;
+tweet *head = NULL;
 
 void create_twitter_system(twitter * twitter_system){
     int num;
@@ -260,49 +260,32 @@ void delete (user * currUser, twitter *twitter_system)
     }
 }
 
-void postTweet(user * currUser, tweet * head, int tweetNum)
+void postTweet (user*currUser,  char tweetArray [USR_LENGHT])
 {
-
     // creates pointer to new node 
-    tweet* newTweetPtr =  malloc(sizeof(tweet)); 
-   // pointer to the previous node created
-    tweet *previousPtr = NULL;
-    tweetNum++;
-    newTweetPtr->id = tweetNum;
-    strcpy(newTweetPtr ->user, currUser ->username);
-    printf("Please enter your message:\n");
-    fgets(newTweetPtr->msg,TWEET_LENGTH,stdin);
+   tweet* newTweetPtr =  malloc(sizeof(tweet)); 
 
 
-//    newTweetPtr ->id =0;
+   newTweetPtr ->id =0;
 
 //copy tweet and username into tweet array
-//    strcpy(newTweetPtr ->msg, tweetArray);
+   strcpy(newTweetPtr ->msg, tweetArray);
+   strcpy(newTweetPtr ->user, currUser ->username);
 
 //update head to newnode
-    if(head == NULL){
-        newTweetPtr->next_tweet = NULL;
-        head = newTweetPtr;
-    }
-    else{
-        previousPtr = head;
-        newTweetPtr ->next_tweet = previousPtr;  
-        head = newTweetPtr; 
-    }
-    printf("posted.\n");
+      newTweetPtr ->next_tweet = head;  
+      head = newTweetPtr; 
 
 }
 
-void getNewsfeed (twitter *twitter_system, tweet * head)
+void getNewsfeed (twitter *twitter_system)
 {
     // assign temp pointer to head of stack
-    tweet *temp = head;
+    // tweet *temp = head;
+    tweet *temp = twitter_system->tweets;
     while(temp != NULL)
     {
-        printf("ID: %d \n",temp->id);
-        printf("User: %s\n", temp->user);
-        printf("Message: %s \n",temp->msg);
-        // printf("%s ->", temp ->msg);
+        printf("%s ->", temp ->msg);
         temp = temp ->next_tweet;
     }
 }
@@ -313,8 +296,8 @@ void menu(twitter *twitter_system){
     printf("please enter:");
     scanf("%s",name);
     user *current_user = findUser(twitter_system,name);
-    // char tweetArray[TWEET_LENGTH];
-    tweet *head = twitter_system->tweets;
+    char tweetArray[TWEET_LENGTH];
+    // tweet * head = twitter_system->tweets;
     
     printf("Please select from one of the following options:\n");
     printf("--------------------------------------------------------\n");
@@ -337,15 +320,20 @@ void menu(twitter *twitter_system){
                 return;
                 break;
             case 1:
-                // printf("tweet ?\n");
-                // fgets(tweetArray, TWEET_LENGTH, stdin);
-                // postTweet(current_user,tweetArray );
-                postTweet(current_user,head,twitter_system->count_tweet);
+                printf("tweet ?\n");
+                fgets(tweetArray, TWEET_LENGTH, stdin);
+                postTweet(current_user,tweetArray );
+                // postTweet(current_user,&head,twitter_system->count_tweet);
+                // tweet *currPtr = head;
+                // while(currPtr!=NULL){
+                //     printf("%s ->\n",head->msg);
+                //     currPtr = currPtr->next_tweet;
+                // }
                 // getNewsfeed();
                 break;
 
             case 2:
-              getNewsfeed(twitter_system,head);
+              getNewsfeed(head);
               break;
 
             case 3:
