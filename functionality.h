edd2eed92f4c04ba3aaba8965686a8a100576a96
sujingdong <sujingdong07@gmail.com>
2022-04-30 02:18:21 +0100

    modules created

--- functionality.h
+++
@@ -1,7 +0,0 @@
-// #include "twitter_create.h"
-
-// void endTurn(user *currentUserPtr);
-// void menu(twitter *twitter_system,char name[USR_LENGHT] );
-// user* findUser(twitter *twitter_system,char name[USR_LENGHT]);
-// void unfollow(twitter *twitter_system,char name[USR_LENGHT]);
-// void follow(user *currUser, twitter *twitter_system);
