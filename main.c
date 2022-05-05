54743b3d2ec3dfadd4ffafc135112f7d2c8baec0
sujingdong <sujingdong07@gmail.com>
2022-05-05 19:04:49 +0100

    minor change

--- main.c
+++ main.c
@@ -7,9 +7,11 @@ int main() {
 
     twitter twitter_system;
     create_twitter_system(&twitter_system);
-
-    printUsers(&twitter_system);
-    menu(&twitter_system);
+     
+    if(twitter_system.users !=NULL){
+        printUsers(&twitter_system);
+        menu(&twitter_system);
+    }
 
     return 0;
 
